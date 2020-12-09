703709c0caa0a3d956e54d0fb37a3c311bd28851
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-12-09 20:04:11 -0300

    conceitos basicos de components

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -8,46 +8,53 @@ import { FRASES } from './frases-mock'
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
+
+
 export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
-  public resposta: string
+  public resposta: string = ''
 
   public rodada: number = 0
   public rodadaFrase: Frase
-  public progresso: number= 0
+  public progresso: number = 0
 
-  constructor() { 
-    this.rodadaFrase= this.frases[this.rodada];
+  constructor() {
+    this.atualizaRodada()
+
   }
 
   ngOnInit(): void {
   }
 
-  atualizaResposta(resposta: Event): void{
+  atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value
-   
   }
 
 
-  verificarResposta(): void{
-     if(this.rodadaFrase.frasePtBr == this.resposta){
-       alert('A tradução está correta')
-       //trocar pergunta da rodada
-       this.rodada++
+  verificarResposta(): void {
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A tradução está correta')
+      //trocar pergunta da rodada
+      this.rodada++
 
-       //progresso
-       this.progresso= this.progresso + (100/ this.frases.length)
-       console.log(this.progresso)
+      //progresso
+      this.progresso = this.progresso + (100 / this.frases.length)
+      console.log(this.progresso)
 
-       //atualiza o objeto rodadaFrase
-       this.rodadaFrase = this.frases[this.rodada];
-    }else{
+      //atualiza o objeto rodadaFrase
+      this.atualizaRodada()
+
+    } else {
       alert('A tradução está errada')
     }
-    
-
-
   }
+  public atualizaRodada(): void {
+    //define a frase da rodada com base em alguma lógica
+    this.rodadaFrase = this.frases[this.rodada];
+    //limpar a resposta
+    this.resposta = ""
+  }
+
 
 }
