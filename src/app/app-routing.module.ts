0ae1ae4dbccb2ea378822b2daf92ffc08cfe0db0
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-12-22 15:20:07 -0300

    configurando tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 import { HomeComponent } from './home/home.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'restaurantes', component: RestaurantesComponent },
   { path: 'diversao', component: DiversaoComponent },
   { path: 'oferta', component: OfertaComponent },
-  { path: 'oferta/:id', component: OfertaComponent },
+  { path: 'oferta/:id', component: OfertaComponent, 
+  children:[
+    { path: '', component: ComoUsarComponent },
+    { path: 'como-usar', component: ComoUsarComponent },
+    { path: 'onde-fica', component: OndeFicaComponent },
+  ] },
 
 ];
 
