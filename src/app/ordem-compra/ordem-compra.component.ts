9052611800718a087052ffbc02206c45a234c4c6
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-12-28 09:42:11 -0300

    criando formulario

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -11,21 +11,46 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = ''
   public formaPagamento: string =''
 
+  //controle de validação dos campos
+  public enderecoValido: boolean
+  public numeroValido: boolean
+  public complementoValido: boolean 
+  public formaPagamentoValido:boolean 
+
   constructor() { }
 
   ngOnInit(): void {
   }
   atualizaEndereco(endereco: string): void{
   this.endereco = endereco
+  //se a string for maior que 3
+  if(this.endereco.length){
+    this.enderecoValido = true
+  }else{
+    this.enderecoValido = false
+  }
   }
   atualizaNumero(numero: string): void{
   this.numero= numero
+    if (this.numero.length > 0) {
+      this.numeroValido = true
+    } else {
+      this.numeroValido = false
+    }
   }
   atualizaComplemento(complemento: string): void{
   this.complemento= complemento
+    if (this.complemento.length > 0) {
+      this.complementoValido = true
+    }
   }
   atualizaFormaPagamento(formaPagamento: string): void{
   this.formaPagamento= formaPagamento
+    if (this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true
+    } else {
+      this.formaPagamentoValido = false
+    }
   }
 
 }
