3b66325c5a78abdaf3e9cb03f0cee88c4bf148f5
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-01-07 09:39:58 -0300

    adicionando favicon

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.scss']
+  styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
 
