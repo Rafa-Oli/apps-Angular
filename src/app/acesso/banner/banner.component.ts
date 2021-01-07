8ed1a5fdc498ad1e5d854b2bf1666f1f4f0720b3
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-01-07 13:50:24 -0300

    trabalhando com animações

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,12 +1,25 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0,
+      })),
+      state('visivel', style({
+        opacity: 1,
+      }))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
 
+  public estado: string = 'escondido'
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
@@ -17,6 +17,7 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     CadastroComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
