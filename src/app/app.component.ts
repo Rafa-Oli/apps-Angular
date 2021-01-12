8c5d779ffd577a07f4561a00ac57a128c55c86f9
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-01-11 21:01:44 -0300

    comunicando com database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app3';
+
+
+  
+  ngOnInit(): void {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDz4oKqFl2ZLCMPMxSEm8HqzJiYCAn-2L8",
+      authDomain: "insta-clone-b60a4.firebaseapp.com",
+      projectId: "insta-clone-b60a4",
+      storageBucket: "insta-clone-b60a4.appspot.com",
+      messagingSenderId: "327084872412",
+      appId: "1:327084872412:web:3e16966051d7720d7fda19",
+      measurementId: "G-LQYDQHL46S"
+
+    };
+
+
+    firebase.default.initializeApp(firebaseConfig);
+    
+  }
 }
--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,8 +1,26 @@
 import { Usuario } from "./acesso/usuario.model";
+import * as firebase from 'firebase';
+
 
 export class Autenticacao{
     public cadastrarUsuario(usuario: Usuario): void{
         console.log('chegamos', usuario)
+
+        firebase.default.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+        .then((resposta: any) =>{
+
+            //remover a senha do atributo senha do obj usuario
+            delete usuario.senha
+            
+            //registrando dados complementares do usuario no path email na base64
+            firebase.default.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+             .set(usuario)
+        
+
+
+        }).catch((error) =>{
+            console.log(error)
+        });
         
     }
 }
