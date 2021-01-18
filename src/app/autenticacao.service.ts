960eb35eaed8ab97b3fc3ff632a4f0db24454596
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-01-18 11:04:13 -0300

    trabalhando autenticacao no login

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -15,11 +15,17 @@ export class Autenticacao{
             //registrando dados complementares do usuario no path email na base64
             firebase.default.database().ref(`usuario_detalhe/${btoa(usuario.email)}`).push(usuario)
         
-
-
         }).catch((error) =>{
             console.log(error)
         });
         
+    }
+
+
+    public autenticar(email: string, senha: string): void{
+        console.log('antes de autenticar')
+        firebase.default.auth().signInWithEmailAndPassword(email,senha)
+        .then((resposta: any) => console.log(resposta))
+        .catch((error: Error) => console.log(error))
     }
 }
